c01ec698c60999dfe5c962e8c5e7169fffce9f71
www <348509930@qq.com>
2019-07-16 00:32:25 +0800

    文件监控对大竹单独判断来显示已处理、未处理

--- app/src/main/java/org/oasystem_dazhu/mvp/view/FileMonitorDelegate.java
+++ app/src/main/java/org/oasystem_dazhu/mvp/view/FileMonitorDelegate.java
@@ -1,7 +1,12 @@
 package org.oasystem_dazhu.mvp.view;
 
 
+import android.view.View;
+import android.widget.LinearLayout;
+
+import org.oasystem_dazhu.BuildConfig;
 import org.oasystem_dazhu.R;
+import org.oasystem_dazhu.constants.Constants;
 import org.oasystem_dazhu.mvp.view.baseDelegate.ViewDelegate;
 
 /**
@@ -22,9 +27,14 @@ public class FileMonitorDelegate extends ViewDelegate {
     @Override
     public void initWidget() {
         getTitleView().setText("文件监控");
+        //大竹没做文件监控的已处理、未处理的分类
+        LinearLayout tabLayout = get(R.id.file_monitor_tab_layout);
+        if (Constants.DAZHU_URL.equals(BuildConfig.HOST)) {
+            tabLayout.setVisibility(View.GONE);
+        }
     }
 
-    public void initMonitorList(){
+    public void initMonitorList() {
 
     }
 }
